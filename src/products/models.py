d8c2015e439fafb3f70cff355e1aaed959dfff88
ManSleen <mike.vansleen@gmail.com>
2019-11-14 09:44:22 -0700

    Deleted db and migrations

--- src/products/models.py
+++ src/products/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Product():
+
+
+class Product(models.Model):
+    title = models.TextField()
+    description = models.TextField()
+    price = models.TextField()
+    summary = models.TextField(default="this is cool!")
--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # THIRD PARTY APPS
+
+    # MY APPS
+    'products',
 ]
 
 MIDDLEWARE = [
